e5c720bf1f2032460db214b8b6a5b59abbe542e4
Polina <ms.maylin@inbox.ru>
2023-01-22 22:34:54 +0300

    Task3 added

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+//
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1, 4.
+
+System.Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+int size = Convert.ToInt32(Console.ReadLine());
+if (size > 0)
+{
+    int[] array = new int[size];
+    int index = 0;
+    while (index < size)
+    {
+        array[index] = index + 1;
+        System.Console.Write($"{Math.Pow(array[index], 2)} ");
+        index++;
+    }
+}
+else
+{
+    System.Console.WriteLine($"Введенное число ({size}) меньше 1.");
+}
