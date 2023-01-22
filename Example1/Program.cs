c47e74648bac3218e193690714c92eeba27c216e
Polina <ms.maylin@inbox.ru>
2023-01-22 20:54:14 +0300

    In Example1: comments added in program.cs (line 3, line 5, line 8, line 10

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Задача: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 
-int[] coords = new int[2];
+int[] coords = new int[2]; // создали массив и выделили память под два элемента в массиве coords
 System.Console.Write("Введите Х: ");
-coords[0] = Convert.ToInt32(Console.ReadLine());
+coords[0] = Convert.ToInt32(Console.ReadLine()); // получили переменную X и записали ее в массив под нулевым индексом
 
 System.Console.Write("Введите Y: ");
-coords[1] = Convert.ToInt32(Console.ReadLine());
+coords[1] = Convert.ToInt32(Console.ReadLine()); // получили переменную Y и записали ее в массив под индексом один
 
-if (coords[0] > 0 && coords[1] > 0)
+if (coords[0] > 0 && coords[1] > 0) // если значение массива с индексом 0 больше нуля и значение массива с индексом 1 больше нуля
 {
     System.Console.WriteLine("Номер четверти - 1");
 }
