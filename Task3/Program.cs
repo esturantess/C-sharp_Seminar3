c5f724171cbe539a27012c955d1bab82c5cae689
Polina <ms.maylin@inbox.ru>
2023-01-22 23:01:20 +0300

    In Task3: another solution added in program.cs

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,22 +3,41 @@
 // 5 -> 1, 4, 9, 16, 25.
 // 2 -> 1, 4.
 
-System.Console.WriteLine("Введите число: ");
-// int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число: ");
+// // int number = Convert.ToInt32(Console.ReadLine());
 
-int size = Convert.ToInt32(Console.ReadLine());
-if (size > 0)
+// int size = Convert.ToInt32(Console.ReadLine());
+// if (size > 0)
+// {
+//     int[] array = new int[size];
+//     int index = 0;
+//     while (index < size)
+//     {
+//         array[index] = index + 1;
+//         System.Console.Write($"{Math.Pow(array[index], 2)} ");
+//         index++;
+//     }
+// }
+// else
+// {
+//     System.Console.WriteLine($"Введенное число ({size}) меньше 1.");
+// }
+
+// Решение без использования массивов:
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+double answer = 0;
+
+if (number > 0)
 {
-    int[] array = new int[size];
-    int index = 0;
-    while (index < size)
+    for (int i = 1; i <= number; i++)
     {
-        array[index] = index + 1;
-        System.Console.Write($"{Math.Pow(array[index], 2)} ");
-        index++;
+        answer = Math.Pow(i,2);
+        System.Console.Write($"{answer} ");
     }
 }
 else
 {
-    System.Console.WriteLine($"Введенное число ({size}) меньше 1.");
+    System.Console.WriteLine("Некорректное число.");
 }
